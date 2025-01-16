3b6720f56b25f1c9cf4128aaee6a970e61548ce8
Mauro Rodriguez <rodriguezmauro1994@gmail.com>
2025-01-16 10:51:18 -0300

    added Koin DI

---
+++ RickAndMortyApp/composeApp/src/commonMain/kotlin/com/maurosergiorodriguez/rickandmortyappmp/di/DomainModule.kt
@@ -0,0 +1,7 @@
+package com.maurosergiorodriguez.rickandmortyappmp.di
+
+import org.koin.dsl.module
+
+val domainModule = module {
+
+}
--- RickAndMortyApp/composeApp/src/iosMain/kotlin/com/maurosergiorodriguez/rickandmortyappmp/MainViewController.kt
+++ RickAndMortyApp/composeApp/src/iosMain/kotlin/com/maurosergiorodriguez/rickandmortyappmp/MainViewController.kt
@@ -1,5 +1,8 @@
 package com.maurosergiorodriguez.rickandmortyappmp
 
 import androidx.compose.ui.window.ComposeUIViewController
+import com.maurosergiorodriguez.rickandmortyappmp.di.initKoin
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController(configure = {
+    initKoin()
+}) { App() }
