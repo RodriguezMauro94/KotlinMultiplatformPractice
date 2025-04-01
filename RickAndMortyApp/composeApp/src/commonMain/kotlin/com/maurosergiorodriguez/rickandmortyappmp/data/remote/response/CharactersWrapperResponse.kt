d2b8710624ff54c12d07d5352767dcd9edc745ee
Mauro Rodriguez <mauro.rodriguez@project-ranger.com>
2025-03-31 23:52:05 -0300

    characters grids

--- RickAndMortyApp/composeApp/src/commonMain/kotlin/com/maurosergiorodriguez/rickandmortyappmp/data/remote/response/CharactersWrapperResponse.kt
+++ RickAndMortyApp/composeApp/src/commonMain/kotlin/com/maurosergiorodriguez/rickandmortyappmp/data/remote/response/CharactersWrapperResponse.kt
@@ -1,10 +1,14 @@
 package com.maurosergiorodriguez.rickandmortyappmp.data.remote.response
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class CharactersWrapperResponse(
     val info: InfoResponse,
     val results: List<CharacterResponse>
 )
 
+@Serializable
 data class InfoResponse(
     val pages: Int,
     val next: String?,
--- RickAndMortyApp/composeApp/src/commonMain/kotlin/com/maurosergiorodriguez/rickandmortyappmp/ui/home/tabs/characters/CharactersScreen.kt
+++ RickAndMortyApp/composeApp/src/commonMain/kotlin/com/maurosergiorodriguez/rickandmortyappmp/ui/home/tabs/characters/CharactersScreen.kt
@@ -1,6 +1,9 @@
 package com.maurosergiorodriguez.rickandmortyappmp.ui.home.tabs.characters
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -8,6 +11,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.GridItemSpan
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
@@ -17,6 +24,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
@@ -25,6 +33,9 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.paging.LoadState
+import app.cash.paging.compose.LazyPagingItems
+import app.cash.paging.compose.collectAsLazyPagingItems
 import coil3.compose.AsyncImage
 import com.maurosergiorodriguez.rickandmortyappmp.domain.model.CharacterModel
 import com.maurosergiorodriguez.rickandmortyappmp.ui.core.ex.vertical
@@ -36,9 +47,95 @@ import org.koin.core.annotation.KoinExperimentalAPI
 fun CharactersScreen() {
     val charactersViewModel = koinViewModel<CharactersViewModel>()
     val state by charactersViewModel.state.collectAsState()
+    val characters = state.characters.collectAsLazyPagingItems()
 
     Column(modifier = Modifier.fillMaxSize()) {
-        CharacterOfTheDay(state.characterOfTheDay)
+        CharactersGridList(state, characters)
+    }
+}
+
+@Composable
+fun CharactersGridList(state: CharactersState, characters: LazyPagingItems<CharacterModel>) {
+    LazyVerticalGrid(
+        modifier = Modifier.fillMaxSize().padding(horizontal = 16.dp),
+        columns = GridCells.Fixed(2),
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        item(span = { GridItemSpan(2) }) {
+            CharacterOfTheDay(state.characterOfTheDay)
+        }
+
+        when {
+            characters.loadState.refresh is LoadState.Loading && characters.itemCount == 0 -> {
+                // Initial load
+                item(span = { GridItemSpan(2) }) {
+                    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                        CircularProgressIndicator(Modifier.size(64.dp), color = Color.Red)
+                    }
+                }
+            }
+            characters.loadState.refresh is LoadState.NotLoading && characters.itemCount == 0 -> {
+                // Empty response
+                item(span = { GridItemSpan(2) }) {
+                    Text("No hay personajes :(")
+                }
+            }
+            else -> {
+                // Fetch items
+                if (characters.loadState.refresh is LoadState.Loading) {
+                    item(span = { GridItemSpan(2) }) {
+                        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                            CircularProgressIndicator(Modifier.size(64.dp), color = Color.Red)
+                        }
+                    }
+                } else {
+                    items(characters.itemCount) { pos ->
+                        characters[pos]?.let { characterModel ->
+                            CharacterItemList(characterModel)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun CharacterItemList(characterModel: CharacterModel) {
+    Box(modifier = Modifier.clip(RoundedCornerShape(24))
+        .border(
+            2.dp,
+            Color.Green,
+            shape = RoundedCornerShape(0, 24, 0, 2)
+        )
+        .fillMaxSize()
+        .clickable {
+
+        },
+        contentAlignment = Alignment.BottomCenter
+    ) {
+        AsyncImage(
+            model = characterModel.image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
+        )
+
+        Box(
+            modifier = Modifier.fillMaxSize().height(60.dp).background(
+                brush = Brush.verticalGradient(
+                    listOf(
+                        Color.Black.copy(0f),
+                        Color.Black.copy(0.6f),
+                        Color.Black.copy(1f)
+                    )
+                )
+            ),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(characterModel.name, color = Color.White, fontSize = 18.sp)
+        }
     }
 }
 
