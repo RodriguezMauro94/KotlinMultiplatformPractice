e5e6af1264667cc871124d51d1ea6b3791c33465
Mauro Rodriguez <rodriguezmauro1994@gmail.com>
2025-04-14 20:08:01 -0300

    Added date example

--- RickAndMortyApp/composeApp/src/commonMain/kotlin/com/maurosergiorodriguez/rickandmortyappmp/domain/GetRandomCharacter.kt
+++ RickAndMortyApp/composeApp/src/commonMain/kotlin/com/maurosergiorodriguez/rickandmortyappmp/domain/GetRandomCharacter.kt
@@ -1,12 +1,29 @@
 package com.maurosergiorodriguez.rickandmortyappmp.domain
 
 import com.maurosergiorodriguez.rickandmortyappmp.domain.model.CharacterModel
+import kotlinx.datetime.Clock
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
 
 class GetRandomCharacter(
     private val repository: Repository
 ) {
     suspend operator fun invoke(): CharacterModel {
+        /*
+        if (getCurrentDayOfTheYear() == characterDatabase.now) {
+            // Fetch db object
+        } else {
+            val randomId = (1 .. 826).random()
+            return repository.getSingleCharacter(randomId.toString())
+        }
+        */
         val randomId = (1 .. 826).random()
         return repository.getSingleCharacter(randomId.toString())
+    }
+
+    private fun getCurrentDayOfTheYear(): Int {
+        val instant = Clock.System.now()
+        val localTime = instant.toLocalDateTime(TimeZone.currentSystemDefault())
+        return localTime.dayOfYear
     }
 }
