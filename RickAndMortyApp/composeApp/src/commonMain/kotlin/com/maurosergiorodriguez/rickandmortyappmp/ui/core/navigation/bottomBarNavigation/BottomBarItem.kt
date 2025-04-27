68e3bccb758347bc1cc1ddab316e93f90f3a5dd1
Mauro Rodriguez <rodriguezmauro1994@gmail.com>
2025-04-27 19:32:49 -0300

    fixed home styles

--- RickAndMortyApp/composeApp/src/commonMain/kotlin/com/maurosergiorodriguez/rickandmortyappmp/ui/core/navigation/bottomBarNavigation/BottomBarItem.kt
+++ RickAndMortyApp/composeApp/src/commonMain/kotlin/com/maurosergiorodriguez/rickandmortyappmp/ui/core/navigation/bottomBarNavigation/BottomBarItem.kt
@@ -1,12 +1,19 @@
 package com.maurosergiorodriguez.rickandmortyappmp.ui.core.navigation.bottomBarNavigation
 
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.maurosergiorodriguez.rickandmortyappmp.ui.core.navigation.Routes
+import org.jetbrains.compose.resources.painterResource
+import rickandmortyapp.composeapp.generated.resources.Res
+import rickandmortyapp.composeapp.generated.resources.ic_characters
+import rickandmortyapp.composeapp.generated.resources.ic_player
 
 sealed class BottomBarItem {
     abstract val route: String
@@ -17,7 +24,7 @@ sealed class BottomBarItem {
         override val route: String = Routes.Episodes.route,
         override val title: String = "Episodes",
         override val icon: @Composable () -> Unit = {
-            Icon(imageVector = Icons.Default.Home, contentDescription = "Episodes")
+            Icon(painter = painterResource(Res.drawable.ic_player), contentDescription = "Episodes", modifier = Modifier.size(24.dp))
         }
     ): BottomBarItem()
 
@@ -25,7 +32,7 @@ sealed class BottomBarItem {
         override val route: String = Routes.Characters.route,
         override val title: String = "Characters",
         override val icon: @Composable () -> Unit = {
-            Icon(imageVector = Icons.Default.Person, contentDescription = "Characters")
+            Icon(painter = painterResource(Res.drawable.ic_characters), contentDescription = "Characters", modifier = Modifier.size(24.dp))
         }
     ): BottomBarItem()
 }
